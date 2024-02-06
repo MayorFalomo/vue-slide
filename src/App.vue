bfc766abd6588c22e928075f79fcf663b7eb8d64
mayor falomo <mayorfalomo@gmail.com>
2024-02-06 23:25:55 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,187 @@
+<script>
+import CardVue from './components/Card.vue'
+import { Icon } from '@iconify/vue';
+export default {
+  name: "App",
+  components: {
+    CardVue,
+    Icon,
+  },
+  data() {
+    return{
+  carousels : [
+    {
+    id: 1,
+    image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    name: "Susan Smith",
+    position: "WEB DEVELOPER",
+    comment: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+          },
+    {
+    id: 2,
+    image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    name: "Anna Johnson",
+    position: "WEB DESIGNER",
+    comment: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal."
+          },
+    
+    {
+    id: 3,
+    image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    name: "Peter Jones",
+    position: "INTERN",
+    comment: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag."
+          },
+    {
+    id: 4,
+    image: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    name: "Bill Anderson",
+    position: "THE BOSS",
+    comment: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic."
+    },
+    ],
+    carouselIndex: 0,
+    }
+    
+  },
+  methods: {
+    handleNext() {
+      this.carouselIndex = (this.carouselIndex + 1) % this.carousels.length;
+    },
+    handlePrevious() {
+      this.carouselIndex = (this.carouselIndex - 1 + this.carousels.length) % this.carousels.length;
+    },
+    pickRandomIndex() {
+      this.carouselIndex = Math.floor(Math.random() * this.carousels.length);
+    },
+  }
+}
+</script>
+
+<template>
+ <div class="wrapper" >
+<div  class="wrappedModal" >
+ <div class="infoContainer" >
+<div class="carouselImg" >
+  <img :src="carousels[carouselIndex].image "  class="carouselImage"  alt="img" />
+</div>
+<h1> {{carousels[carouselIndex].name}} </h1>
+<span> {{carousels[carouselIndex].position}} </span>
+<p> {{carousels[carouselIndex].comment}} </p>
+</div>
+<div class="controlBtn" >
+<span @click="handleNext " ><Icon icon="uim:angle-left" width="50px" height="50px" /></span>
+<span @click="handlePrevious " ><Icon icon="uim:angle-right"  width="50px" height="50px" /> </span>
+</div>
+<button @click="pickRandomIndex " class="surpriseBtn" > Surprise Me </button>
+</div>
+ </div>
+ <CardVue/>
+</template>
+
+<style scoped>
+.wrapper{
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #F8FAFC;
+}
+.wrappedModal{
+   height: fit-content;
+   max-height: 800px;
+   width: 60%;
+   max-width: 900px;
+   min-width: 350px;
+    background-color: white;
+  border-radius: 20px;
+  padding: 20px;
+box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+}
+.infoContainer{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
+.carouselImg{
+    position: relative;
+    width: 230px;
+    height: 230px;
+    background-color: #645CFF;
+    border-radius: 50%;
+}
+.carouselImg span{
+  background: #645CFF;
+  color: white;
+  position: absolute;
+  left: 0;
+  top: 20px ;
+  z-index: 2;
+  font-size: 35px;
+}
+
+.carouselImage{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    position: absolute;
+    left: -15px;
+    top: 0;
+    z-index: 1;
+    background-color: #645CFF;
+    border-radius: 50%;
+}
+
+.infoContainer h1{
+  font-size: 30px;
+  font-weight: 500;
+  margin-top: 10px;
+}
+.infoContainer span{
+  font-size: 20px;
+  color: #735CFF;
+}
+.infoContainer p{
+  font-size: 20px;
+  line-height: 35px;
+  width: 95%;
+  text-align: center;
+}
+.controlBtn{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+}
+.controlBtn span{
+  cursor: pointer;
+}
+.surpriseBtn{
+  display: flex;
+  justify-content: center;
+  font-size: 18px;
+  border: none;
+  outline: none;
+  padding: 10px 20px;
+  border-radius: 8px;
+  margin: 20px auto;
+  background-color: #C1BEFF;
+  color: #735CFF;
+  cursor: pointer;
+}
+
+@media (max-width: 600px) {
+  .wrappedModal{
+    width: 95%;
+  }
+  .carouselImg{
+    width: 150px;
+    height: 150px;
+   
+}
+}
+</style>
+645CFF
---
+++ src/components/Card.vue
@@ -0,0 +1,169 @@
+<template>
+<div class="cardContainer" >
+<div class="faqModal" > 
+    <h1> Questions </h1>
+    <div class="faqContainer" >
+    <div class="faqSection" > 
+        <div :style="flexTexts " >
+        <p class="question" >Do I Have To Allow The Use Of Cookies? </p>
+        <span v-if="numberZero == 0" @click="numberZero = 1 " :style="styleBtn " > + </span>
+        <span v-if="numberZero == 1" @click="numberZero = 0" :style="styleBtn "  > - </span>
+        </div>
+        <p v-if="numberZero == 1" class="faqAnswer" >Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art. </p>
+    </div>
+    <div class="faqSection" > 
+        <div :style="flexTexts" >
+        <p class="question" >How Do I Change My My Page Password? </p>
+        <span v-if="numberOne == 1" @click="numberOne = 2 " :style="styleBtn "  > + </span>
+        <span v-if="numberOne == 2" @click="numberOne = 1" :style="styleBtn "  > - </span>
+        </div>
+        <p v-if="numberOne == 2"  class="faqAnswer"  >Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse. </p>
+    </div>
+    <div class="faqSection" > 
+        <div :style="flexTexts" >
+        <p class="question" >What is Bank Id? </p>
+         <span v-if="numberTwo == 2" @click="numberTwo = 3 "  :style="styleBtn "  > + </span>
+        <span v-if="numberTwo == 3" @click="numberTwo = 2"  :style="styleBtn "  > - </span>
+        </div>
+        <p v-if="numberTwo == 3"  class="faqAnswer"  >Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial. </p>
+    </div>
+    <div class="faqSection" > 
+        <div :style="flexTexts" >
+        <p class="question" >Whose Birth Number Can I Use? </p>
+         <span v-if="numberThree == 3" @click="numberThree = 4" :style="styleBtn "  > + </span>
+        <span v-if="numberThree == 4" @click="numberThree = 3" :style="styleBtn "  > - </span>
+        </div>
+        <p  v-if="numberThree == 4"  class="faqAnswer"  >Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art. </p>
+    </div>
+    <div class="faqSection" > 
+        <div :style="flexTexts" >
+        <p class="question" >When Do I Recieve A Password Ordered By Letter? </p>
+         <span  v-if="numberFour == 4" @click="numberFour = 5" :style="styleBtn "  > + </span>
+        <span  v-if="numberFour == 5" @click="numberFour = 4" :style="styleBtn "  > - </span>
+        </div>
+        <p  v-if="numberFour == 5"  class="faqAnswer"  >Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan </p>
+    </div>
+    <div class="faqSection" > 
+        <div :style="flexTexts" >
+        <p class="question" >Do I Have To Allow The Use Of Cookies? </p>
+         <span  v-if="numberFive == 5" @click="numberFive = 6" :style="styleBtn "  > + </span>
+        <span  v-if="numberFive == 6" @click="numberFive = 5" :style="styleBtn " > - </span>
+        </div>
+        <p  v-if="numberFive == 6"  class="faqAnswer"  >Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art. </p>
+    </div>
+    </div>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+props: {
+    carousel: Object
+},
+data() {
+    return {
+        numberZero: 0,
+        numberOne: 1,
+        numberTwo: 2,
+        numberThree: 3,
+        numberFour: 4,
+        numberFive: 5,
+        styleBtn : {
+ fontSize: '22px',
+    minWidth: '30px',
+    minHeight: '30px',
+    backgroundColor: '#164E63',
+    color: 'white',
+    borderRadius: '50%',
+    display: 'grid',
+    placeContent: 'center',
+    cursor: 'pointer',
+        },
+        flexTexts : {
+             display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '15px',
+    width: '100%',
+        }
+    }
+},
+}
+</script>
+
+<style>
+.cardContainer{
+   width: 100vw;
+   min-height: 100vh;
+   overflow: hidden;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   margin: 0 auto;
+   background-color: #CFFAFE;
+   font-family: "Inclusive Sans";
+}
+.faqModal {
+    width: 60%;
+    height: fit-content;
+    /* max-height: 700px; */
+     border-radius: 20px;
+  padding: 20px;
+  background-color: white;
+box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+}
+.faqModal h1{
+    text-align: center;
+   font-size: 30px;
+}
+.faqContainer{
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    align-items: center;
+    margin: 30px auto;
+    width: 90%;
+}
+.faqSection{
+    width: 100%;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+
+}
+
+.faqSection .question{
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 40px;
+   padding: 10px;
+   color: #334155;
+}
+.faqSection .faqAnswer{
+    font-size: 18px;
+    line-height: 30px;
+   width: 95%;
+   margin: 0 auto;
+   color: #000;
+   opacity: 0.8;
+}
+
+@media (max-width: 1200px) {
+    .faqModal{
+        width: 80%;
+    }
+}
+@media (max-width: 850px) {
+    .faqModal{
+        width: 95%;
+    }
+}
+@media (max-width: 500px) {
+    .faqModal{
+        width: 100%;
+        border-radius: 0;
+    }
+    .faqContainer{
+        width: 100%;
+    }
+}
+</style>
